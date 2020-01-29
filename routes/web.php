6813c4650c9de9f8831f5b050d5ce87d10eeda39
Yosuke Suzuki <pvc100kie@gmail.com>
2020-01-29 08:22:37 +0000

    retake

--- routes/web.php
+++ routes/web.php
@@ -60,4 +60,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', 'NewsController@index');
-Route::get('/', 'ProfileController@index');
+//Route::get('/', 'ProfileController@index');
